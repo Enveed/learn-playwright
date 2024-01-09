079fe2fb08d1d446633af1b434f1830de5bc7026
Enveed <sopagnarithseam@gmail.com>
2024-01-09 22:26:01 +0700

    fix pytest fixture not setup properly

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,6 +10,9 @@ def create_context(browser: Browser):
 
     page.locator("#user-name").fill("standard_user")
     page.locator("#password").fill("secret_sauce")
+    page.locator("input[type='submit']").click()
+
+    expect(page).to_have_url("https://www.saucedemo.com/inventory.html")
 
     context.storage_state(path=".auth/auth.json")
     context.close()
